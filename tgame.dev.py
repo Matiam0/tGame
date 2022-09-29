1cc45afd597b2f185d421e152a06b379086ca6ae
ArttieF <arttief+github@gmail.com>
2022-09-29 03:23:53 -0500

    Restarting
    Want to sit down and rethink how I handle this.

--- tgame.dev.py
+++ tgame.dev.py
@@ -1,27 +1,9 @@
-import src.tlib as t
-import json
+from TGame.src import tlib
 
 def main():
     #credits/opening text from file.
-    textlib = t.loadfile("src/textlib.json")
-    sleeptime = 3
-    print(textlib.get('ocredit'))
-    t.sleep(sleeptime)
-    print(textlib.get('ocredit-1'))
-    t.sleep(sleeptime)
-    print(textlib.get('ocredit-2'))
-    t.sleep(sleeptime)
-    print(textlib.get('ocredit-3'))
-    t.sleep(sleeptime)
-    print(textlib.get('ocredit-4'))
-    t.sleep(sleeptime)
+    print()
     
-    #Character creation.
-    t.clear()
-    sheet = t.ccreate()
-    t.clear()
-    t.scs(sheet)
-    t.sleep(5)
     
 
 main()
