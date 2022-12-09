8bd9d71eae452a790cefd2afe0ce9f4cb9320f2a
ArttieF <arttief+github@gmail.com>
2022-12-09 14:30:04 -0600

    Changes to be committed: 	modified:   lib/lib.json         Added Centipedal Dolphins, and Halvodr as races, updated versioning  to 0.1.14 	modified:   lib/tlib.py         Added in t.title() and commented required files for  this function to work. 	modified:   tgame.py         Added first support itteration for race selection.

--- lib/tlib.py
+++ lib/tlib.py
@@ -1,3 +1,13 @@
+#!Read Me
+
+#This library requires the following files
+##A "lib\lib.json" with the version and gamestate key:variable pairs.
+##If you want the above to not be required you can comment out the def title, ... lines.
+
+
+
+
+
 # Use for System type commands that can be used in the lib later.
 # define easy to use clear screen that works on multiple os
 def clear():
@@ -13,6 +23,20 @@ def wait(n):
     sleep(n)
 
 # RPG def below here
+
+##Game functions
+def title():
+    LibJson = "lib\lib.json"
+    GameVersion = fromJsonKey(LibJson,'version')
+    GameState = fromJsonKey(LibJson,'gamestate')
+    if GameState == "dev":
+        Dic = {"line1":f"tGame [Dev] {GameVersion}","line2":"_______________________","line3":""}
+    elif GameState == "troll":
+        Dic = {"line1":f"tGame [Troll] {GameVersion}","line2":"_______________________","line3":""}
+    else:
+        Dic = {"line1":f"tGame {GameVersion}","line2":"_______________________","line3":""}
+    for v in Dic.values():
+        print(v)
 
 ##Json management functions
 
--- tgame.py
+++ tgame.py
@@ -1,29 +1,23 @@
 from lib import tlib as t
 # Easy to edit Game; Version, Title. While Adding in Hooks for Json Text Libraries.
 LibJson = "lib\lib.json"
-GameVersion = t.fromJsonKey(LibJson,'version')
-GameState = t.fromJsonKey(LibJson,'gamestate')
-if GameState == "dev":
-    GameTitle = {"line1":f"tGame [Dev] {GameVersion}","line2":"_______________________","line3":""}
-elif GameState == "troll":
-    GameTitle = {"line1":f"tGame [Troll] {GameVersion}","line2":"_______________________","line3":""}
-else:
-    GameTitle = {"line1":f"tGame {GameVersion}","line2":"_______________________","line3":""}
 name="pc"
 gender="male"
 age="19"
 
 
-def main(GameTitle, LibJson):
-#Assigning Wait Values for ease of access.
+def main(LibJson):
+    #ToDo: Make main menu
+
+    #Assigning Wait Values for ease of access.
     WaitShort = 1
     WaitMedium = 5
     WaitMild = 10
     WaitLong = 20
-#Clear screen to be sure, then use printVal for GameTitle and Version.
+    #Clear screen to be sure, then use printVal for GameTitle and Version.
     t.clear()
-    t.printVal(GameTitle)
-#Credits/opening text from file.
+    t.title()
+    #Credits/opening text from file.
     GameOpening = t.fromJsonKey(LibJson, 'intro')
     print(GameOpening.get('1'))
     t.wait(WaitShort)
@@ -41,12 +35,12 @@ def main(GameTitle, LibJson):
     t.wait(WaitMild)
 
     
-#ToDo:Character Creation
-##Gender
+    #ToDo:Character Creation
+    ##Gender
     hold = 1
     while hold == 1:
         t.clear()
-        t.printVal(GameTitle)
+        t.title()
         gender = str(input("Are you male or female? : ")).lower()
         if gender == "male" or gender == "female" or gender == "m" or gender == "f":
             if gender == "m" or gender == "male":
@@ -63,12 +57,12 @@ def main(GameTitle, LibJson):
         t.clear()
         
     
-##Age
+    ##Age
     hold = 1
     while hold == 1:
         try:
             t.clear()
-            t.printVal(GameTitle)
+            t.title()
             age = int(input("How old are you? : "))
             if age != 0:
                 if age >= 0:
@@ -80,28 +74,50 @@ def main(GameTitle, LibJson):
             t.wait(WaitShort)
         else:
             t.clear()
-            t.printVal(GameTitle)
+            t.printVal()
 
-##ToDo: Race selection.
+    ##ToDo: Race selection.
+    races = t.fromJsonKey(LibJson, 'races')
+    hold = 1
+    while hold == 1:
+        try:
+            t.clear()
+            t.title()
+            print(f"{races.get('1')}, {races.get('2')}, {races.get('3')}, {races.get('4')}, or {races.get('5')}")
+            race = input(f"Select a race from the above. ")
+            if race != races.get('1') or races.get('2') or races.get('3') or race.get('4') or race.get('5'):
+                hold = 1
+            else:
+                hold = 0
+        except ValueError:
+            t.clear()
+            t.title()
+            print("You either misspelled the race, input something wrong, or you forgot to capitalize the first letter.")
+            t.wait(WaitShort)
+            print("Try again.")
+            t.wait(WaitMedium)
+        else:
+            t.clear()
+            t.title()
+            
+    ##Todo: Generate initial stats, distribute starting points.
     
-##Todo: Generate initial stats, distribute starting points.
-    
-##ToDo: Make dictionary called sheet to hold stats age, name, and race.
+    ##ToDo: Make dictionary called sheet to hold stats age, name, and race.
 
-##ToDo: XP/Level system.
+    ##ToDo: XP/Level system.
 
     t.clear()
-## Credits
-    t.printVal(GameTitle)
-    print(name, gender, age)
+    ## Credits
+    t.printVal()
+    print(name, gender, age, race)
     GameCredits = t.fromJsonKey(LibJson, 'credits')
     print(GameCredits.get('1'))
     t.wait(WaitShort)
     print(GameCredits.get('2'))
     t.wait(WaitMedium)
-#Finale screen clear.
+    #Finale screen clear.
     t.wait(WaitLong)
     t.clear()
         
 
-main(GameTitle, LibJson)
+main(LibJson)
