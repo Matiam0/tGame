aed0935aa789c6e35b8fd76039aab21dcb6f31ba
ArttieF <arttief@gmail.com>
2023-07-12 10:11:59 -0700

    Code Cleanup.

--- tgame.py
+++ tgame.py
@@ -2,11 +2,11 @@ from random import randint
 
 class game():
     def rollDice(sides):
-        roll = randint(1, sides)
+        roll = randint(1, sides) # type: ignore
         return roll
     
     def fromDic(dic):
-        for i,v in dic:
+        for i,v in dic: # type: ignore
             print(f"{i}:{v}")
 
     class charInit():
@@ -31,7 +31,7 @@ class game():
                 }
             }
         }
-        def create(status=statusDefault):
+        def create(statusDefault):
             print()
             
 
@@ -40,20 +40,20 @@ class tFormat():
         return "\33[{code}m".format(code=code)
 
     def fClear():
-        print(tFormat.fCode(0))
+        print(tFormat.fCode(0)) # type: ignore
 
     def cJustify(text, width=-1):
-      lines = text.split('\n')
+      lines = text.split('\n') # type: ignore
       width = max(map(len, lines)) if width == -1 else width
       return '\n'.join(line.center(width) for line in lines)
 
     def rJustify(text,width=-1):
-        lines = text.split('\n')
+        lines = text.split('\n') # type: ignore
         width = max(map(len,lines)) if width == -1 else width
         return '\n'.join(line.rjust(width) for line in lines)
 
     def fTitle(text,width=100,bg="40",st="31",cc="1"):
-        print(tFormat.fCode(bg) + tFormat.fCode(st) + tFormat.fCode(cc),end="")
-        text = tFormat.fCode(bg) + tFormat.fCode(st) + tFormat.fCode(cc) + text
+        print(tFormat.fCode(bg) + tFormat.fCode(st) + tFormat.fCode(cc),end="") # type: ignore
+        text = tFormat.fCode(bg) + tFormat.fCode(st) + tFormat.fCode(cc) + text # type: ignore
         print(tFormat.cJustify(text,width))
-        tFormat.cclear()
+        tFormat.cclear() # type: ignore
