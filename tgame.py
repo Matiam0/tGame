29f94ec180787b525fdb351772fafbdec44bde2f
Matiamo <63567491+Matiam0@users.noreply.github.com>
2024-03-11 15:18:54 +0000

    Planning phase and brainstorming.

--- tgame.py
+++ tgame.py
@@ -1,31 +1,36 @@
+
+##!Import above this line.
+##!Text Formatting options go in this class.
 class format():
-    def color():
+
+    def color(text,color): ##Todo Add support for 16 colors
         print()
 
-    def font():
+    def font(text,font): ##Todo Add support for at least two fonts.
         print()
 
-    def align():
+    def align(text,alignment): ##Todo Add Left, Center, and Right alignment options.
         print()
 
-
+##!Character functions go in this class.
 class character():
-    def create():
+
+    def create(): ##Todo Character creation will go here, may opt to merge this with edit into a sub-class later.
         print()
 
-    def edit():
+    def edit(): ##Todo Edit characters, aka use skill points and change stats.
         print()
 
-    def save():
+    def save(): ##Todo Make basic Save/Load for characters.
         print()
 
     def load():
         print()
 
-
+##! Anything not falling into the above classes will go here as game functions.
 class game():
 
-    def dice():
+    def dice(): ##Todo Roll a dice with x number of sides and return the number.
         print()
 
 
